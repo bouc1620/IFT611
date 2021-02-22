3d7fff83fcc6343ac979a940e79e575459eedf9e
Nicholas Vézina <como89.dev@gmail.com>
2021-02-22 09:05:08 -0500

    Fix private fields not supported by firefox

--- Client/js/data.js
+++ Client/js/data.js
@@ -1,37 +1,25 @@
 class Character {
-    #id_user
-    #char
+    id_user
+    char
     /* 
     Array with position column only [1, 0]
     1 represent the column position in the editor
     0 represent the position of character in array (if need to be between two character)
     */
-    #position 
+    position 
 
     constructor(id_user,char,position) {
-        this.#id_user = id_user;
-        this.#char = char;
-        this.#position = position;
-    }
-
-    get user() {
-        return this.#id_user;
-    }
-
-    get position() {
-        return this.#position;
-    }
-
-    get char() {
-        return this.#char;
+        this.id_user = id_user;
+        this.char = char;
+        this.position = position;
     }
 }
 
 class Document {
-    #doc_array
+    doc_array
 
     constructor() {
-        this.#doc_array = [];
+        this.doc_array = [];
     }
 
     /*
@@ -39,7 +27,7 @@ class Document {
     */
     addCharacterLocal(id_user, char, position) 
     {
-        var line_array = this.#doc_array[position.line];
+        var line_array = this.doc_array[position.line];
 
         if (line_array === undefined) {
 
@@ -48,7 +36,7 @@ class Document {
             var position_gen = [position.ch,0]; // TODO : support position between two characters
             column_array.push(new Character(id_user,char[0],position_gen)) // TODO : support multiple characters
             line_array.push(column_array);
-            this.#doc_array.push(line_array);
+            this.doc_array.push(line_array);
         } else {
             var column_array = line_array[position.ch];
             
