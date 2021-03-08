0c8b05365b747bc787c0c566c583bb9566aa1261
Charles Boudreault <bouc1620@usherbrooke.ca>
2021-03-07 22:47:18 -0500

    ajout de commentaires et réusinage du code

--- Client/js/data.js
+++ Client/js/data.js
@@ -1,3 +1,7 @@
+const MIN = 1; // 2^0
+const MID = 32768; // 2^15
+const MAX = 1073741824; // 2^30
+
 class Character {
   constructor (char, pos, user) {
     this.char = char;
@@ -5,7 +9,7 @@ class Character {
     this.user = user;
   }
 
-  static compare (char1, char2) {
+  static comparePos (char1, char2) {
     for (let i = 0; i < Math.min(char1.pos.length, char2.pos.length); i++) {
       if (char1.pos[i] > char2.pos[i]) {
         return 1;
@@ -28,11 +32,69 @@ class Character {
 
     return 0;
   }
-}
 
-const MIN = 1; // 2^0
-const MID = 32768; // 2^15
-const MAX = 1073741824; // 2^30
+  /**
+   * Generates a position array in between the before and after arguments
+   * @param {Character.pos} before 
+   * @param {Character.pos} after 
+   * @returns {Character.pos} newPos
+   */
+  static genPosBetween (before, after) {
+    let newPos;
+    if (before) {
+      if (after) {
+        if (before[0] + 1 == after[0]) {
+          if (before.length == after.length) {
+            if (before.length == 1) {
+              newPos = [before[0], MID];
+            } else if (before[before.length - 1] << 1 == after[after.length - 1]) {
+              newPos = Object.values(before);
+              newPos.push(MID);
+            } else {
+              newPos = Object.values(before);
+              newPos[newPos.length - 1] <<= 1;
+            }
+          } else if (before.length > after.length) {
+            newPos = Object.values(before);
+            if (before[before.length - 1] == MAX) {
+              newPos.push(MID);
+            } else {
+              newPos[newPos.length - 1] <<= 1;
+            }
+          } else {
+            newPos = Object.values(after);
+            if (after[after.length - 1] == MIN) {
+              newPos.push(MID);
+            } else {
+              newPos[newPos.length - 1] >>>= 1;
+            }
+          }
+        } else {
+          newPos = [before[0] + 1];
+        }
+      } else {
+        newPos = [before[0] + 1];
+      }
+    } else if (after) {
+      if (after.length == 1) {
+        if (after[0] > 1) {
+          newPos = [after[0] - 1];
+        } else {
+          newPos = [0, MID];
+        }
+      } else if (after[after.length - 1] == MIN) {
+        newPos = Object.values(after);
+        newPos.push(MID);
+      } else {
+        newPos = Object.values(after);
+        newPos[newPos.length - 1] >>>= 1;
+      }
+    } else {
+      newPos = [1];
+    }
+    return newPos;
+  }
+}
 
 class Document {
   constructor () {
@@ -61,76 +123,11 @@ class Document {
     this.processBacklog();
   }
 
-  processBacklog () {
-    let exists = false;
-    for (let [_log, list] of Object.entries(this.backlog)) {
-      if (list.length > 0) {
-        console.debug(list);
-        exists = true;
-      }
-    }
-
-    // TODO, process the operations received before the document initialization
-
-    console.debug(`processBacklog, not implemented yet. there is ${exists ? 'a' : 'no'} backlog`);
-  }
-
   insert_fromLocal (char, index, user) {
-    const before = index != 0 ? this.document[index - 1] : null;
-    const after = this.document.length > index ? this.document[index] : null;
+    const before = index != 0 ? this.document[index - 1].pos : null;
+    const after = this.document.length > index ? this.document[index].pos : null;
 
-    let pos;
-    if (before) {
-      if (after) {
-        if (before.pos[0] + 1 >= after.pos[0]) {
-          if (before.pos.length == after.pos.length) {
-            if (before.pos.length == 1) {
-              pos = [before.pos[0], MID];
-            } else if (before.pos[before.pos.length - 1] << 1 == after.pos[after.pos.length - 1]) {
-              pos = Object.values(before.pos);
-              pos.push(MID);
-            } else {
-              pos = Object.values(before.pos);
-              pos[pos.length - 1] <<= 1;
-            }
-          } else if (before.pos.length > after.pos.length) {
-            pos = Object.values(before.pos);
-            if (before.pos[before.pos.length - 1] == MAX) {
-              pos.push(MID);
-            } else {
-              pos[pos.length - 1] <<= 1;
-            }
-          } else {
-            pos = Object.values(after.pos);
-            if (after.pos[after.pos.length - 1] == MIN) {
-              pos.push(MID);
-            } else {
-              pos[pos.length - 1] >>>= 1;
-            }
-          }
-        } else {
-          pos = [before.pos[0] + 1];
-        }
-      } else {
-        pos = [before.pos[0] + 1];
-      }
-    } else if (after) {
-      if (after.pos.length == 1) {
-        if (after.pos[0] > 1) {
-          pos = [after.pos[0] - 1];
-        } else {
-          pos = [0, MID];
-        }
-      } else if (after.pos[after.pos.length - 1] == MIN) {
-        pos = Object.values(after.pos);
-        pos.push(MID);
-      } else {
-        pos = Object.values(after.pos);
-        pos[pos.length - 1] >>>= 1;
-      }
-    } else {
-      pos = [1];
-    }
+    let pos = Character.genPosBetween(before, after);
 
     const newChar = new Character(char, pos, user);
 
@@ -148,25 +145,10 @@ class Document {
       return;
     }
 
-    let low = 0;
-    let high = this.document.length - 1;
-    let k = 0;
-    while (low <= high) {
-      k = (low + high) >>> 1;
-      let compare = Character.compare(char, this.document[k]);
+    let index = this.findCharIndex(char);
 
-      if (compare == 1) {
-        low = k + 1;
-      } else if (compare == -1) {
-        high = k - 1;
-      } else {
-        console.error('error, trying to insert same character twice');
-        return;
-      }
-    }
-
-    this.document.splice(high + 1, 0, char);
-    editor.codemirror.replaceRange(char.char, editor.codemirror.posFromIndex(high + 1));
+    this.document.splice(index, 0, char);
+    editor.codemirror.replaceRange(char.char, editor.codemirror.posFromIndex());
   }
 
   replace_fromLocal (char, pos, user) {
@@ -193,6 +175,50 @@ class Document {
     }
 
     // TODO
+  }
+
+  /**
+   * Processes the operation backlog containing the operations received while the peer
+   * was waiting for a copy of the document
+   */
+  processBacklog () {
+    let exists = false;
+    for (let [_log, list] of Object.entries(this.backlog)) {
+      if (list.length > 0) {
+        console.debug(list);
+        exists = true;
+      }
+    }
+
+    // TODO, process the operations received before the document initialization
+
+    console.debug(`processBacklog, not implemented yet. there is ${exists ? 'a' : 'no'} backlog`);
+  }
+
+  /**
+   * Performs a binary search to find either the Character's correct insertion index in the
+   * document array or the Character's actual index if it was found in the array
+   * @param {Character} char 
+   * @returns {number} index
+   */
+  findCharIndex (char) {
+    let low = 0;
+    let high = this.document.length - 1;
+
+    while (low <= high) {
+      let k = (low + high) >>> 1;
+      let compare = Character.comparePos(char, this.document[k]);
+
+      if (compare == 1) {
+        low = k + 1;
+      } else if (compare == -1) {
+        high = k - 1;
+      } else {
+        return k;
+      }
+    }
+
+    return high + 1;
   }
 }
 
